457cbfd8b5d3465b67ceb7a0e17a0b0ed201554f
CaminDzN <camidzgranados@hotmail.com>
2025-04-08 01:07:26 -0500

    Cambio dirección IP

--- dags/covertype_workflow.py
+++ dags/covertype_workflow.py
@@ -55,7 +55,7 @@ def collect_covertype_data():
 
     # Pedimos datos a random-data-api (internamente lo llamamos: http://random-data-api:8001/data)
     for group_number in range(1, 11):
-        url = f"http://10.43.101.172:8001/data?group_number={group_number}"
+        url = f"http://192.168.1.30:8001/data?group_number={group_number}"
         resp = requests.get(url)
         if resp.status_code == 200:
             result = resp.json()
